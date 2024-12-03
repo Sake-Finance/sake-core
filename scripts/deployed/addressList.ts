67599f5b97bf5eacbe05c30dd3b609a0f3e78454
ChunWang1998 <jjooee1998@gmail.com>
2024-12-03 10:23:18 +0800

    chore: addressList

--- scripts/deployed/addressList.ts
+++ scripts/deployed/addressList.ts
@@ -1,3 +1,3 @@
-export const poolConfiguratorAddress = "0x80aE159B7bE1a82baE38389D7df266e512B611E6";
-export const poolAddress = "0x3f35d5E6feF03a03a91eE4F051A2860CdD8C633c";
-export const dataProviderAddress = "0x2532b697496EEBfC2674A8c6215A48Bb0eaf5b22"
+export const poolConfiguratorAddress = "0xC253A91c0405FEE71304d135Aa3CAEc30CEe5A34";
+export const poolAddress = "0xEc38a5Cd88E87Fec0D10822DE8a3D6dB144931DA";
+export const dataProviderAddress = "0x90032d7A63467485B95a91596576f5C2dE88BdB3"
